ba0d6471e788f0e52da21d48b82dd6162768e9fd
alex lee <leol3o05@gmail.com>
2025-11-18 15:03:00 +0900

    basic basic connection

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -17,4 +18,33 @@ func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddr,
 	}
+}
+
+// Public function, start with uppercase not lowercase
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+	return nil
+}
+
+// Private function, start with lowercase not uppercase
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %v\n", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("new incoming connection %v\n", conn)
 }
--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -10,4 +10,7 @@ func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
 	assert.Equal(t, tr.listenAddress, listenAddr)
+
+	// Server
+	assert.Nil(t, tr.ListenAndAccept())
 }
